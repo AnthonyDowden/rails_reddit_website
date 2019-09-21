ffd26ae64c227ad1eba0169dc381c8d42b478536
AnthonyDowden <a.dowden@outlook.com>
2019-09-21 22:29:40 +0100

    comments and posts done

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
 
 	root to:'home#index'
 
-	resources :communities
-	resources :posts do
-	  resources :comments
+	resources :communities do
+		resources :posts do
+			resources :comments
+		end
 	end
-
 end
